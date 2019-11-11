bf32e9b6387fca4eb460efb28808ac04e2ec56ca
samak842 <57339010+samak842@users.noreply.github.com>
2019-11-11 15:16:55 +0500

    Add files via upload

---
+++ resources/views/fb.blade.php
@@ -0,0 +1,233 @@
+<html>
+	<head>
+		<title>
+			Facebook
+		</title>
+		<style type="text/css">
+			
+			}
+			.inpuut{
+				padding: 5px;
+				margin-top:15px;
+				border: 1px solid #bdc7d8;
+				border-radius:7px;
+				width: 250px;
+				height:35px;
+			}
+			
+			.input{
+				padding: 5px;
+				margin-top:15px;
+				border: 1px solid #bdc7d8;
+				border-radius:7px;
+				width: 230px;
+				height:40px;
+			}
+
+
+
+			
+			.select{
+				margin-top:25px;
+				border: 1px solid #bdc7d8;
+				border-radius:7px;
+				margin-right: :80px;
+				height: 35px;
+				width:100%; 
+			}
+			
+			.sellect{
+						margin-top:25px;
+						border: 1px solid #bdc7d8;
+						border-radius:7px;
+						margin-right: :80px;
+						height: 35px;
+						width:20%; 
+			}
+			
+			.submittt{
+				border: 0px;
+				border-radius: 4px;
+				display: inline-block;
+				color: white;
+				background-color:green;
+				padding: 4px;
+				width: 110px;
+				height: 30px;}
+			
+		</style>
+	</head>
+	<body>
+		<table border="0px" cellspacing="0" width="100%" height="100%">
+			<tr height="80" bgcolor="#3B5998" style="background-repeat:no-repeat; background-size:cover;">
+				<td background="td1.PNG">
+					
+				</td>
+				
+				<td width="45">
+					<form>
+						<table>
+							<tr>
+								<td width="180" style="color:white"> 
+									<p>
+										Email or Phone
+									</p>
+								</td>
+								
+								<td colspan="2"  style="color:white">
+									Password
+								</td>
+							</tr>
+							
+							<tr> 
+								<td class="input1">
+									<input type="email" name="email" class="input1">
+								</td>
+									
+								<td>
+									<input type="password" name="email" class="input1">
+								</td>
+								
+								<td>
+									<input type="submit" class="submitt" value="Login">
+								</td>
+							</tr>
+						</table>
+					</form>
+						
+				</td>
+				
+			</tr>
+			
+			<tr>
+				<td width="60%" background="fb-bg.PNG" style="background-repeat:no-repeat; background-size:cover;" >
+				</td>
+				
+				<td background="bgcolor.PNG" style="background-repeat:no-repeat; background-size:cover;padding: 0px;">
+					<from>
+					<table style="margin-left:5%;height: 100%; display: inline-block ;"> 
+						<tr>
+							<td colspan="2">
+								<h1>Sign up</h1>
+								<h2> Its free and anyone can join</h2>
+								<br/>				
+							</td>		
+						</tr>
+						<tr>
+							<td>
+								Full name
+								<input type="text" placeholder="First name" class="input">
+							</td>
+							<td>
+								<input type="text" name="Email" class="input" style="margin-top:31px" placeholder="Last name">
+							</td>
+						</tr>
+						<tr>
+							<td> 
+							</br>Email or Phonen
+								<input type="text" class="input" placeholder="Mobile number or Email" >
+							</td>
+						</tr>
+						<tr>
+							<td>New Password<br>
+								<input type="password" placeholder="New password"  class="input">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<table >
+										<tr>
+											Date of Birth
+											<td>
+												<select name="Year" class="select">
+													<option values="january">2017<br/></option>
+													<option values="feburary">2016<br/></option>
+													<option values="march">2015<br/></option>
+													<option values="april">2014<br/></option>
+													<option values="may">2013<br/></option>
+													<option values="june">2012<br/></option>
+													<option values="jully">2011<br/></option>
+													<option values="august">2005<br/></option>
+													<option values="september">2004<br/></option>
+													<option values="octuber">2003<br/></option>
+													<option values="november">2002<br/></option>
+													<option values="december">2001</option><br/>
+												</select>
+											</td>
+											<td>
+												<select name="month" class="select">
+													<option values="january">January<br/></option>
+													<option values="feburary">Feburary<br/></option>
+													<option values="march">march<br/></option>
+													<option values="april">april<br/></option>
+													<option values="may">may<br/></option>
+													<option values="june">june<br/></option>
+													<option values="jully">jully<br/></option>
+													<option values="august">august<br/></option>
+													<option values="september">september<br/></option>
+													<option values="octuber">octuber<br/></option>
+													<option values="november">november<br/></option>
+													<option values="december">december</option><br/>
+												</select>
+											</td>
+						
+											<td>
+												<select name="days" class="select">
+													<option value="1">1<br/></option>
+													<option value="2">2<br/></option>
+													<option value="3">3<br/></option>
+													<option value="4">4<br/></option>
+													<option value="5">5<br/></option>
+													<option value="6">6<br/></option>
+													<option value="7">7<br/></option>
+													<option value="8">8<br/></option>
+													<option value="9">9<br/></option>
+													<option value="10">10<br/></option>
+													<option value="11">11<br/></option>
+													<option value="12">12<br/></option>
+													<option value="13">13<br/></option>
+													<option value="14">14<br/></option>
+													<option value="15">15<br/></option>
+													<option value="16">16<br/></option>
+													<option value="17">17<br/></option>
+													<option value="18">18<br/></option>
+													<option value="19">19<br/></option>
+													<option value="20">20<br/></option>
+													<option value="21">21<br/></option>
+													<option value="22">22<br/></option>
+													<option value="23">23<br/></option>
+													<option value="24">24<br/></option>
+													<option value="25">25<br/></option>
+													<option value="26">26<br/></option>
+													<option value="27">27<br/></option>
+													<option value="28">28<br/></option>
+													<option value="29">29<br/></option>
+													<option value="30">30<br/></option>
+													<option value="31">31<br/></option>
+												</select><br>
+					
+											</td>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								Gender
+								<select name="gender" class="sellect">
+								<option class="select" values="Female">Male<br/></option>
+								<option class="select" values="Male">Female<br/></option>
+								</select><br>
+
+							</td>
+						</tr>
+						
+						<tr>
+							<td>
+								<input type="submit" name="submit" value="Sign up" class="submittt">
+							</td>
+						</tr>
+						</table>
+					</form>
+			
+	</body>
+</html>
